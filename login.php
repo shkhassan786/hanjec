34add2aba2d792fc857ecd9573bed104e3f99edc
HASSAN SHAIKH <hassanshaikh77866@gmail.com>
2025-10-13 06:11:12 +0530

    cnf
    cnf

--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'database.php';
-$email = $_POST['email'];
-$password = $_POST['password'];
-$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
-$result = mysqli_query($conn,$sql);
-if(mysqli_num_rows($result)>0){echo "Login successful";} 
-else {echo "Invalid credentials";}
-?>
--- signup.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'database.php';
-$email = $_POST['email'];
-$password = $_POST['password'];
-$sql = "INSERT INTO users (email,password) VALUES ('$email','$password')";
-if(mysqli_query($conn,$sql)){echo "Signup successful";} 
-else {echo "Error: ".mysqli_error($conn);}
-?>
